bd0fa1403e34a61c3c921c5b06492c6fea721fc0
stijnbilliet <stijn.billiet@student.howest.be>
2018-10-04 09:16:52 +0200

    Got assimp to build

--- Projects/Framework/Content/Model/Model.cpp
+++ Projects/Framework/Content/Model/Model.cpp
@@ -1,6 +1,10 @@
 #include "FrameworkPCH.h"
 #include "Model.h"
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 Model::Model(const char* modelPath)
 	:m_ModelPath(modelPath)
 {
